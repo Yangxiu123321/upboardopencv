04c84e3dc8f598a1111e791a89571120a9b24bc0
Neu <20164278@stu.neu.edu.cn>
2017-10-29 12:39:58 +0800

    图像模糊

--- opencv/opencv/opencv/main.cpp
+++ opencv/opencv/opencv/main.cpp
@@ -1,17 +1,21 @@
-#include <opencv2/opencv.hpp>
+//#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
 using namespace cv;
 using namespace std;
 void main()
 {
-	Mat srcImage = imread("E:\\opencvstu\\picture\\dog.jpg");
+	Mat srcImage = imread("E:\\opencvstu\\picture\\dog.bmp");
 	imshow("¡¾Ô­Ê¼Í¼¡¿",srcImage);
-	Mat element = getStructuringElement(MORPH_RECT,Size(15,15));
+	Mat element = getStructuringElement(MORPH_RECT,Size(10,10));
 	Mat dstImage;
 	//¸¯Ê´
-	erode(srcImage,dstImage,element);
+	//erode(srcImage,dstImage,element);
 	//ÅòÕÍ
 	//dilate(srcImage,dstImage,element);
-	imshow("¡¾¸¯Ê´Í¼¡¿",dstImage);
+	//ÂË²¨
+	blur(srcImage,dstImage,Size(7,7));
+	imshow("¡¾¾ùÖµÂË²¨Í¼¡¿",dstImage);
 	waitKey(0);
 }
