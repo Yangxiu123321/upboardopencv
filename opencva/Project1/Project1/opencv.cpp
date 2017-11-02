732e7e8fe8e04498a8dee50ef16aa32980dabfcf
Neu <20164278@stu.neu.edu.cn>
2017-11-02 21:27:04 +0800

    FindCoutours

--- opencva/Project1/Project1/opencv.cpp
+++ opencva/Project1/Project1/opencv.cpp
@@ -1,45 +1,28 @@
-﻿#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <opencv2\core\core.hpp>
-#include <opencv2\highgui\highgui.hpp>
-#include <opencv2\imgproc\imgproc.hpp>
-
-using namespace std;
+﻿#include "opencv2/opencv.hpp"
+#include "opencv2/highgui.hpp"
+#include <iostream>
+#include "stdlib.h"
+#include "stdio.h"
 using namespace cv;
-
-int main(int argc, char **argv)
+using namespace std;
+int main(int argc, char*argv[])
 {
-	const int num = 200;
-	char fileName[250];
-	char windowName[250];
-	Mat srcImage;
-
-	int64 timeCount = getTickCount();		//int64  typedef long long int64
-
-	for (int i = 1; i <= num; i++)
-	{
-		sprintf_s(fileName, "E:\\sample\\neg\\n_s%d.png", i);
-		sprintf_s(windowName, "NO.%d", i);
-
-		//按照图像名称读取图像
-		srcImage = imread(fileName);
-		//if (!srcImage.data)
-		if (srcImage.empty())
-		{
-			cout << "文本为空!" << endl;
-			exit(EXIT_FAILURE);
-		}
-		resize(srcImage, srcImage, Size(200, 200));
-		namedWindow(windowName);
-		imshow(windowName, srcImage);
-
-		cout << "NO. " << i << " " << endl;
-	}
-	//timeCount = (double)(getTickCount() - timeCount) / (getTickFrequency());
-	timeCount = static_cast<int64>((getTickCount() - timeCount) / getTickFrequency());
-	cout << "读取200张图片共用时： " << timeCount << " s" << endl;
+	Mat srcImage, grayImage, imgColor, contoursImage;
+	srcImage = imread("E:\\opencvstu\\picture\\dog.jpg",0);
+	if (!srcImage.data) { cout << "read Error!\r\n"; return -1; }
+	//cvtColor(srcImage,grayImage,COLOR_BGR2GRAY);
+	//GaussianBlur(grayImage, grayImage, Size(3, 3), 3, 3);
+	////blur(grayImage,grayImage,Size(3,3));
+	threshold(srcImage, grayImage,100,255, CV_THRESH_OTSU);
+	imshow("1", srcImage);
+	//bitwise_not(grayImage,grayImage);//黑白图像反转
+	//对图像进行二值化
+	imshow("2", grayImage);
+	vector<vector<Point>>contours;
+	vector<Vec4i>hierarchy;
+	findContours(grayImage,contours, hierarchy,CV_RETR_CCOMP,CV_CHAIN_APPROX_NONE);
+	Mat resultImage = Mat::zeros(srcImage.size(), CV_8U);
+	drawContours(resultImage, contours, -1, Scalar(255, 0, 255));
+	imshow("轮廓图像", resultImage);
 	waitKey(0);
-
-	return 0;
 }
